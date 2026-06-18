f3edc7c12847bc93625e0e1dbb5c53a891b3ec04
william-francia <wfranciapoma@byupathway.edu>
2026-06-17 23:44:36 -0400

    changes

--- js/main.js
+++ js/main.js
@@ -91,38 +91,3 @@ document
         );
 
     });
-
-const topBar =
-    document.querySelector(
-        ".top-bar"
-    );
-
-const header =
-    document.querySelector(
-        ".header"
-    );
-
-window.addEventListener(
-    "scroll",
-    () => {
-
-        if (window.scrollY > 50) {
-
-            topBar.style.transform =
-                "translateY(-35px)";
-
-            header.style.top = "0";
-
-        }
-
-        else {
-
-            topBar.style.transform =
-                "translateY(0)";
-
-            header.style.top = "35px";
-
-        }
-
-    }
-);
